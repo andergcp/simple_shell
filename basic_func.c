86dd39aeaedf37d0091edb796c049e5e7387c037
AlejoCasti <acastiblanco8@gmail.com>
2020-04-04 16:26:28 +0000

    [FIX]: betty detail

--- basic_func.c
+++ basic_func.c
@@ -14,23 +14,20 @@ void exe_command(char *arg)
 	ptr = malloc(len + 1);
 	if (!ptr)
 		return;
-	else
+	ansFo = fork();
+	if (ansFo == -1)
+		return;
+	if (ansFo != 0)
+		sleep(1);
+	if (ansFo == 0)
 	{
-		ansFo = fork();
-		if (ansFo == -1)
-			return;
-		if (ansFo != 0)
-			sleep(1);
-		if (ansFo == 0)
-		{	
-			while (arg[ansExe] != '\n')
-				ptr[ansExe] = arg[ansExe], ansExe++;
-			ptr[len] = '\0';
-			argv[0] = ptr, argv[1] = NULL;
-			x = execve(ptr, argv, NULL);
-			printf("Error\n");
-			exit(0);
-		}	
+		while (arg[ansExe] != '\n')
+			ptr[ansExe] = arg[ansExe], ansExe++;
+		ptr[len] = '\0';
+		argv[0] = ptr, argv[1] = NULL;
+		x = execve(ptr, argv, NULL);
+		printf("Error\n");
+		exit(0);
 	}
 	free(ptr);
 }

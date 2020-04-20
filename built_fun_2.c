2487f1344b91e10cf615c2f3fa373b7006f0cea4
AlejoCasti <acastiblanco8@gmail.com>
2020-04-20 17:49:54 +0000

    [FIX] setenv and unsetenv features

--- built_fun_2.c
+++ built_fun_2.c
@@ -8,6 +8,8 @@ int comm_unset(variables *m_v)
 {
 	env_v *tmp, *en = m_v->p_env;
 
+	if (!(m_v->args[1]))
+		return (0);
 	if (!get_env(m_v, m_v->args[1]))
 		return (0);
 	if (_strcmp(en->name, m_v->args[1]) == 0)
@@ -43,7 +45,9 @@ int comm_unset(variables *m_v)
  */
 int comm_set(variables *m_v)
 {
-	if (!(m_v->args[2]) || !(m_v->args[1]))
+	if (!(m_v->args[1]))
+		return (0);
+	if (!(m_v->args[2]))
 		return (0);
 	set_env(m_v, m_v->args[1], m_v->args[2]);
 	return (0);
